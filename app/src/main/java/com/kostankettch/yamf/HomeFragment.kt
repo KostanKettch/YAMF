239dac94518f07d5487c822741a228515581669e
KostanKettch <kostankettch@gmail.com>
2022-05-01 00:03:15 +0400

    Animation added

--- app/src/main/java/com/kostankettch/yamf/HomeFragment.kt
+++ app/src/main/java/com/kostankettch/yamf/HomeFragment.kt
@@ -1,13 +1,17 @@
 package com.kostankettch.yamf
 
 import android.os.Bundle
+import androidx.transition.*
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.transition.TransitionSet
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.merge_home_screen_content.*
 import java.util.*
 
 class HomeFragment : Fragment() {
@@ -97,6 +101,18 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val scene = Scene.getSceneForLayout(home_fragment_root, R.layout.merge_home_screen_content, requireContext())
+        val searchSlide = Slide(Gravity.TOP).addTarget(R.id.search_view)
+        val recyclerSlide = Slide(Gravity.BOTTOM).addTarget(R.id.main_recycler)
+        val disclaimerFade = Fade().addTarget(R.id.disclaimer)
+        val customTransition = TransitionSet().apply {
+            duration = 500
+            addTransition(recyclerSlide)
+            addTransition(searchSlide)
+            addTransition(disclaimerFade)
+        }
+        TransitionManager.go(scene, customTransition)
 
         search_view.setOnClickListener {
             search_view.isIconified = false
