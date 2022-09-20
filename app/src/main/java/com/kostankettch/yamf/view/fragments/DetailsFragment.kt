607829f331306562caa0b4a4abb36816bc36c053
KostanKettch <kostankettch@gmail.com>
2022-09-21 02:07:34 +0400

    Module_42 Coroutines implemented

--- app/src/main/java/com/kostankettch/yamf/view/fragments/DetailsFragment.kt
+++ app/src/main/java/com/kostankettch/yamf/view/fragments/DetailsFragment.kt
@@ -1,20 +1,37 @@
 package com.kostankettch.yamf.view.fragments
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
+import androidx.fragment.app.viewModels
+import com.kostankettch.yamf.data.entity.Cinema
 import com.kostankettch.yamf.R
 import com.kostankettch.yamf.data.ApiConstants
 import com.kostankettch.yamf.databinding.FragmentDetailsBinding
-import com.kostankettch.yamf.data.entity.Cinema
+import com.kostankettch.yamf.viewmodel.DetailFragmentViewModel
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
+
 
 class DetailsFragment : Fragment() {
     private lateinit var cinema: Cinema
     private lateinit var binding: FragmentDetailsBinding
+    private val viewModel: DetailFragmentViewModel by viewModels()
+    private val scope = CoroutineScope(Dispatchers.IO)
 
 
     override fun onCreateView(
@@ -49,7 +66,10 @@ class DetailsFragment : Fragment() {
             )
             intent.type = "text/plain"
             startActivity(Intent.createChooser(intent, "Share To:"))
+        }
 
+        binding.detailsFabDownloadWp.setOnClickListener {
+            performAsyncLoadOfPoster()
         }
     }
 
@@ -68,4 +88,84 @@ class DetailsFragment : Fragment() {
             else R.drawable.ic_round_star_border_24
         )
     }
+
+    private fun performAsyncLoadOfPoster() {
+        if (!checkPermission()) {
+            requestPermission()
+            return
+        }
+        MainScope().launch {
+            binding.progressBar.isVisible = true
+            val job = scope.async {
+                viewModel.loadWallpaper(ApiConstants.IMAGE_URL + "original" + cinema.poster)
+            }
+            saveToGallery(job.await())
+            Snackbar.make(
+                binding.root,
+                R.string.downloaded_to_gallery,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(R.string.open) {
+                    val intent = Intent()
+                    intent.action = Intent.ACTION_VIEW
+                    intent.type = "image/*"
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+                .show()
+            binding.progressBar.isVisible = false
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1
+        )
+    }
+
+    private fun saveToGallery(bitmap: Bitmap) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val contentValues = ContentValues().apply {
+                put(MediaStore.Images.Media.TITLE, cinema.title.handleSingleQuote())
+                put(
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    cinema.title.handleSingleQuote()
+                )
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CinemaSearchApp")
+            }
+            val contentResolver = requireActivity().contentResolver
+            val uri = contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            val outputStream = contentResolver.openOutputStream(uri!!)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream?.close()
+        } else {
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.insertImage(
+                requireActivity().contentResolver,
+                bitmap,
+                cinema.title.handleSingleQuote(),
+                cinema.description.handleSingleQuote()
+            )
+        }
+    }
+
+    private fun String.handleSingleQuote(): String {
+        return this.replace("'", "")
+    }
+
 }
